b4bc4e28f87de8b8ecfaa57491c93afff8263533
eparkhurst <eli.parkhurst@gmail.com>
2016-02-23 10:11:27 -0700

    d

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 var Express = require("express")
 var Socket = require("socket.io")
 var http = require("http")
-var unirest = require('unirest')
-var db = require('monk')('localhost/realState')
+//var unirest = require('unirest')
+//var db = require('monk')('localhost/realState')
 
 var app = Express()
 var server = http.Server(app)
