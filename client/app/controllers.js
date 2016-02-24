d6b7e9607362ebea8dc9d2fe3287e04e758752ff
eparkhurst <eli.parkhurst@gmail.com>
2016-02-23 17:07:04 -0700

    details page is hooked up

--- client/app/controllers.js
+++ client/app/controllers.js
@@ -6,7 +6,7 @@ function MainController($scope, DataService, LiveService){
   var socket = io()
   socket.on("info", function(data){
     $scope.servers = data.data
-    //console.log(data.data);
+    console.log(data.data);
     $scope.$apply()
   })
 }
@@ -17,7 +17,7 @@ function DetailController($scope, LiveService, $stateParams){
   socket.on("info", function(data){
     for (var i = 0; i < data.data.length; i++) {
       if(parseInt(data.data[i].id) ===parseInt(id)){
-        $scope.details = data.data;
+        $scope.details = data.data[i];
       }
     }
     $scope.$apply()
